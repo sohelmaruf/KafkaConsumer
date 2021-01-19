737fb16bb000595be50668afe3d50146578177a1
sohelmaruf <almaruf@hotmail.com>
2021-01-19 09:57:03 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
